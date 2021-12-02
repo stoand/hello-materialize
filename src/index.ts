e3bf098d9f20527243b5b5f2d64199c3f9261e4a
Andreas Stocker <shadowstep7@gmail.com>
2021-12-02 09:31:40 +0100

    can run query

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { Client } from 'ts-postgres';
+import { Client, Query } from 'ts-postgres';
 
 export const run = async () => {
     let client = new Client({ user: 'materialize', port: 6875 });
     await client.connect();
 
+    let query = new Query('SELECT 5');
+    let result = await client.execute(query);
     
-    console.log('asdf3');
+    console.log(result.rows[0][0]);
 };
