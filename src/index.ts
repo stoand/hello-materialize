397719109ad3818ef8700f0f8632373d76a07f2d
Andreas Stocker <shadowstep7@gmail.com>
2021-12-02 09:53:36 +0100

    can insert and select values

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 import { Client, Query } from 'ts-postgres';
 
 export const run = async () => {
-    let client = new Client({ user: 'materialize', port: 6875 });
+    let client = new Client({
+        user: 'materialize', port: 6875, database: 'materialize'
+    });
+
     await client.connect();
 
-    let query = new Query('SELECT 5');
-    let result = await client.execute(query);
-    
-    console.log(result.rows[0][0]);
-};
+    let q = async (queryStr: string): Promise<any> => {
+        let query = new Query(queryStr);
+        return await client.execute(query);
+    };
+
+
+    console.log(await q(`
+        DROP TABLE IF EXISTS symbols
+    `));
+
+    console.log(await q(`
+        CREATE TABLE symbols (
+            symbol text,
+            ticker text
+        )
+    `));
+
+
+    console.log(await q(`
+        INSERT INTO symbols
+        SELECT *
+        FROM (VALUES ('Apple','AAPL'),
+                     ('Google','GOOG'),
+                     ('Elerium','ELER'),
+                     ('Bespin Gas','BGAS'),
+                     ('Linen Cloth','LCLO')
+        )
+    `));
+
+    console.log(await q(`SELECT * FROM symbols`));
+}
