c64d1e1f555f1c3cf32c6dca82e290443ea92df9
Andreas Stocker <shadowstep7@gmail.com>
2021-12-02 08:51:30 +0100

    setup typescript compilation and kiwi tests

---
+++ src/tests.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import { asdf } from './index';
+
+describe('First Module', () => {
+
+    it('', () => {});
+    
+    it('this should work', () => {
+        expect(1).to.equal(1);
+    });
+    
+    it('this should be ignored', () => {
+        console.log(2);
+    });
+});
